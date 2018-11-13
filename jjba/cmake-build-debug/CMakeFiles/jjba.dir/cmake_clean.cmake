c1fd6f862e603b908dd51435f46b075484b7aee1
Alex Tarng <alecadad@gmail.com>
2018-11-13 14:46:01 -0500

    Initial commit

---
+++ jjba/cmake-build-debug/CMakeFiles/jjba.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jjba.dir/main.cpp.obj"
+  "jjba.pdb"
+  "jjba.exe"
+  "jjba.exe.manifest"
+  "jjba.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jjba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ jjba/cmake-build-debug/CMakeFiles/jjba.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jjba.
+# This may be replaced when dependencies are built.
