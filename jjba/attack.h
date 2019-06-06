5442326ba7f097e3d16bfd287cc3c93390a1a982
Alex Tarng <alecadad@gmail.com>
2019-06-05 23:25:40 -0400

    created attack.h

--- jjba/attack.h
+++ jjba/attack.h
@@ -5,4 +5,15 @@
 #ifndef JJBA_ATTACK_H
 #define JJBA_ATTACK_H
 
+#include "object.h"
+
+class Attack : public Object {
+public:
+    //Constructor
+    Attack(Vector position, std::vector<Hitbox> mHitboxes);
+
+    //Member functions
+};
+
+
 #endif //JJBA_ATTACK_H
